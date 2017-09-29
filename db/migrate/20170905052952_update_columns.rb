f21f59a5135a7780a1dd9b5ff40f9a0d373c257a
Rodrigo Aguilar <rodrigoaguilar887@gmail.com>
2017-09-28 19:26:36 -0700

    rails migration bug fix for PG database

--- db/migrate/20170905052952_update_columns.rb
+++ db/migrate/20170905052952_update_columns.rb
@@ -2,7 +2,8 @@ class UpdateColumns < ActiveRecord::Migration[5.1]
   def change
 		change_table :highscores do |t|
 			#t.change :time, :float
-			t.change :time, 'float USING CAST(time AS float)'
+			#t.change :time, 'float USING CAST(time AS float)'
 		end
+		change_column :highscores, :time, 'float USING CAST(time AS float)'
   end
 end
