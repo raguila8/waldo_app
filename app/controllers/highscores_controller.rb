6ad7061efe3f86e1645968bfe3ec967be8448fe7
Rodrigo Aguilar <rodrigoaguilar887@gmail.com>
2017-09-28 19:59:30 -0700

    fix another bug in highscores#create action and configure app for production

--- app/controllers/highscores_controller.rb
+++ app/controllers/highscores_controller.rb
@@ -6,7 +6,7 @@ class HighscoresController < ApplicationController
 
 		@highscore = Highscore.new(name:params[:name], time: params[:time], image_id: params[:image_id])
 		@highscore.save
-		highscores = Highscore.order(:time)
+		highscores = Highscore.where(image_id: @highscore.image_id).order(:time)
 		if highscores.length > 5
 			highscores.last.delete
     end
